711a2f086b94cc217e8802c33a42ce56e11fdf34
Luiz Calixto <calixtoluiz0@gmail.com>
2024-07-12 19:58:53 -0300

    P4 - Criando Classe Operacoes

--- src/java/com/filterflix/Enum/Operacoes.java
+++ src/java/com/filterflix/Enum/Operacoes.java
@@ -15,31 +15,7 @@ public enum Operacoes {
 
     @Override
     public String toString() {
-        switch (this) {
-            case COMECAR:
-                return "Comecar";
-            case PAUSAR:
-                return "Pausar";
-            case PARAR:
-                return "Parar";
-            case AVANCO_RAPIDO:
-                return "Avanço Rapido";
-            case RETROCEDER:
-                return "Retroceder";
-            case PROXIMO:
-                return "Próximo";
-            case ANTERIOR:
-                return "Anterior";
-            case ALTERAR_LEGENDA:
-                return "Alterar Legenda";
-            case ALTERAR_AUDIO:
-                return "Alterar Audio";
-            case PROXIMO_EPISODIO:
-                return "Próximo Episódio";
-            case EPISODIO_ANTERIOR:
-                return "Episódio Anterior";
-            default:
-                throw new IllegalArgumentException();
+        switch(this) {
         }
     }
 }
