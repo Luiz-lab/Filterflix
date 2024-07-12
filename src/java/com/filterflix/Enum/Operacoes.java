4ef20b35ac56ce099bd26ded28843d1bfdd1ba2c
Luiz Calixto <calixtoluiz0@gmail.com>
2024-07-12 20:05:29 -0300

    Finalizando alteracao pt_br

--- src/java/com/filterflix/Enum/Operacoes.java
+++ src/java/com/filterflix/Enum/Operacoes.java
@@ -16,6 +16,19 @@ public enum Operacoes {
     @Override
     public String toString() {
         switch(this) {
+            case COMECAR: return "Play";
+            case PAUSAR: return "Pause";
+            case PARAR: return "Stop";
+            case AVANCO_RAPIDO: return "Avanço Rapido";
+            case RETROCEDER: return "Retroceder";
+            case PROXIMO: return "Próximo";
+            case ANTERIOR: return "Previous";
+            case ALTERAR_LEGENDA: return "Change Subtitle";
+            case ALTERAR_AUDIO: return "Change Audio";
+            case PROXIMO_EPISODIO: return "Next Episode";
+            case EPISODIO_ANTERIOR: return "Previous Episode";
+            default: throw new IllegalArgumentException();
         }
     }
 }
+
