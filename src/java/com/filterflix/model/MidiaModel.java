86df8eea1d08a33987f2365ae8bcf2e96a8917db
Luiz Calixto <calixtoluiz0@gmail.com>
2024-07-13 18:46:40 -0300

    P6 - Metodo catalogo usando stream e lambdas

--- src/java/com/filterflix/model/MidiaModel.java
+++ src/java/com/filterflix/model/MidiaModel.java
@@ -1,108 +1,38 @@
 package java.com.filterflix.model;
 
-
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.List;
 
-@SuppressWarnings("ALL")
-
+@Data
 @Getter
 @Setter
+@NoArgsConstructor
 public abstract class MidiaModel {
-    private String titulo;
-    private String genero;
-    private List<GeneroModel> generos = new ArrayList<>();
+    private String nome;
+    private ArrayList<GeneroModel> generos;
     private double avaliacao;
     private String classificacao;
     private String diretor;
     private LocalDate lancamento;
-    private String sinopse;
-    private int duracao;
 
-    public MidiaModel(String titulo, ArrayList<GeneroModel> generos, double avaliacao, String classificacao, String diretor, LocalDate lancamento) {
-        this.titulo = titulo;
-        this.genero = String.valueOf(generos);
+    public MidiaModel(String nome, ArrayList<GeneroModel> generos, double avaliacao, String classificacao, String diretor, LocalDate lancamento) {
+        this.nome = nome;
+        this.generos = generos;
         this.avaliacao = avaliacao;
         this.classificacao = classificacao;
         this.diretor = diretor;
         this.lancamento = lancamento;
-    }
-
-    public MidiaModel() {
-
     }
 
     public abstract void assistir();
 
-    public List<GeneroModel> getGeneros() {
+    public ArrayList<GeneroModel> getGeneros() {
         return generos;
     }
 
-
     public void imprimirMidia() {
-    }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public void setAvaliacao(double avaliacao) {
-        this.avaliacao = avaliacao;
-    }
-
-    public String getClassificacao() {
-        return classificacao;
-    }
-
-    public void setClassificacao(String classificacao) {
-        this.classificacao = classificacao;
-    }
-
-    public String getDiretor() {
-        return diretor;
-    }
-
-    public void setDiretor(String diretor) {
-        this.diretor = diretor;
-    }
-
-    public LocalDate getLancamento() {
-        return lancamento;
-    }
-
-    public void setLancamento(LocalDate lancamento) {
-        this.lancamento = lancamento;
-    }
-
-    public String getSinopse() {
-        return sinopse;
-    }
-
-    public void setSinopse(String sinopse) {
-        this.sinopse = sinopse;
-    }
-
-    public double getDuracao() {
-        return duracao;
-    }
-
-    public double getAvaliacao() {
-        return avaliacao;
     }
 }
